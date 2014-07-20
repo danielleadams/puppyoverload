1cd64a7d2907aba4e51b2260df5b7180fa3c36e6
Danielle Adams <dadams18@gmail.com>
2014-07-20 15:17:14 -0400

    make puppy images smaller

--- overloadBrowser.js
+++ overloadBrowser.js
@@ -41,8 +41,8 @@ var puppyGenerator = {
   },
   setPuppyLayout: function(photo) {
     photo.setAttribute('style', 'border: 1px solid black; margin: 5px');
-    photo.setAttribute('width', '148px');
-    photo.setAttribute('height', '148px');
+    photo.setAttribute('width', '143px');
+    photo.setAttribute('height', '143px');
   },
   reloadPuppies: function() {
     document.addEventListener('scroll', function(event) {
