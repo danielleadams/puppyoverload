cd951f2252f839484428bc7606147643a1157f9b
Danielle Adams <dadams18@gmail.com>
2014-07-20 14:08:18 -0400

    add border and margin to puppy pictures

--- overloadBrowser.js
+++ overloadBrowser.js
@@ -27,6 +27,7 @@ var puppyGenerator = {
       var img = document.createElement('img');
       img.src = this._constructPuppyURL(puppies[i]);
       img.setAttribute('alt', puppies[i].getAttribute('title'));
+      img.setAttribute('style', 'border: 1px solid black; margin: 5px');
       document.body.appendChild(img);
     }
   },
@@ -36,7 +37,7 @@ var puppyGenerator = {
         '/' + photo.getAttribute('id') +
         '_' + photo.getAttribute('secret') +
         '_q.jpg';
-  },
+  }
   reloadPuppies: function() {
     document.addEventListener('scroll', function(event) {
       if (document.body.scrollHeight == document.body.scrollTop + window.innerHeight) {
